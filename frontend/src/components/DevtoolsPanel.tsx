60680691d96d967dc559973405a8ae4c8c82fd73
Nils Hartmann <nils@nilshartmann.net>
2025-10-24 09:36:53 +0200

    devtools in eigener komponente

---
+++ frontend/src/components/DevtoolsPanel.tsx
@@ -0,0 +1,23 @@
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+
+export default function DevtoolsPanel() {
+  return (
+    <TanStackDevtools
+      config={{
+        position: "bottom-right",
+      }}
+      plugins={[
+        {
+          name: "Tanstack Router",
+          render: <TanStackRouterDevtoolsPanel />,
+        },
+        {
+          name: "Tanstack Query",
+          render: <ReactQueryDevtoolsPanel />,
+        },
+      ]}
+    />
+  );
+}
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -4,20 +4,16 @@ import {
   createRootRouteWithContext,
   retainSearchParams,
 } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
-
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import appCss from "../styles.css?url";
 
 import type { QueryClient } from "@tanstack/react-query";
+import DevtoolsPanel from "@/components/DevtoolsPanel.tsx";
 
 interface MyRouterContext {
   queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  notFoundComponent: () => <div>Schade, nicht gefunden!</div>,
   head: () => ({
     meta: [
       {
@@ -28,7 +24,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "🍩 Donuitgram",
+        title: "Donuitgram",
       },
     ],
     links: [
@@ -63,21 +59,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         }
       >
         {children}
-        <TanStackDevtools
-          config={{
-            position: "bottom-right",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            {
-              name: "Tanstack Query",
-              render: <ReactQueryDevtoolsPanel />,
-            },
-          ]}
-        />
+        <DevtoolsPanel />
         <Scripts />
       </body>
     </html>
